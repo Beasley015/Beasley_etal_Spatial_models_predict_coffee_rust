4e829e13f58e239bc8461c91070039aa5ce64a05
eribue <42782508+eribue@users.noreply.github.com>
2019-04-27 22:16:24 -0400

    ups

--- LandscapeSimResults.R
+++ LandscapeSimResults.R
@@ -12,4 +12,29 @@ output.list <- lapply(filenames, read.csv, header = F)
 # Rename columns of each dataframe
 newnames <- c("Time", "PercInf")
 output.list <- lapply(output.list, setNames, newnames)
+head(output.list)
 
+#Libraries
+library(tidyverse)
+library(ggplot2)
+#adding columns defors and disp
+defors <- c("10")
+disp<- c("2")
+for( i in seq_along(output.list)){
+  output.list[[i]]$defors <- rep(defors[i],nrow(output.list[[i]]))
+  output.list[[i]]$disp<-rep(disp[i], nrow(output.list[[i]]))
+  }
+print(output.list[[1]])
+
+library(stringr)
+
+library(tools)
+files<-file_path_sans_ext(list.files(pattern = "*.csv"))
+as.vector(files)
+files
+
+output.list[[1]]
+
+
+
+    
