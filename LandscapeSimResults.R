e7bfe53a6852855038fcdc0ad56663c82f7a1903
Beasley015 <beasley.em@gmail.com>
2019-04-29 14:37:28 -0400

    update r script

--- LandscapeSimResults.R
+++ LandscapeSimResults.R
@@ -4,9 +4,14 @@
 # Spring 2019                                       #
 #####################################################
 
+#Libraries
+library(tidyverse)
+library(ggplot2)
+
 # Read in model outputs --------------------------------------
 # Read all csv's into a list
-filenames <- list.files("Outputs", pattern = "*.csv", full.names = T)
+filenames <- list.files("Outputs", pattern = "*.csv", filenames = T)
+shortnames <- list.files("Outputs", pattern = "*.csv")
 output.list <- lapply(filenames, read.csv, header = F)
 
 # Rename columns of each dataframe
@@ -14,26 +19,7 @@ newnames <- c("Time", "PercInf")
 output.list <- lapply(output.list, setNames, newnames)
 head(output.list)
 
-#Libraries
-library(tidyverse)
-library(ggplot2)
-#adding columns defors and disp
-defors <- c("10")
-disp<- c("2")
-for( i in seq_along(output.list)){
-  output.list[[i]]$defors <- rep(defors[i],nrow(output.list[[i]]))
-  output.list[[i]]$disp<-rep(disp[i], nrow(output.list[[i]]))
-  }
-print(output.list[[1]])
 
-library(stringr)
-
-library(tools)
-files<-file_path_sans_ext(list.files(pattern = "*.csv"))
-as.vector(files)
-files
-
-output.list[[1]]
 
 
 
