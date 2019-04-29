c7e9d558cbcf313d02d6b2df051aca16080ef741
Beasley015 <beasley.em@gmail.com>
2019-04-29 14:44:48 -0400

    added replicates

--- LandscapeSimResults.R
+++ LandscapeSimResults.R
@@ -10,17 +10,19 @@ library(ggplot2)
 
 # Read in model outputs --------------------------------------
 # Read all csv's into a list
-filenames <- list.files("Outputs", pattern = "*.csv", filenames = T)
+filenames <- list.files("Outputs", pattern = "*.csv", full.names = T)
 shortnames <- list.files("Outputs", pattern = "*.csv")
 output.list <- lapply(filenames, read.csv, header = F)
 
 # Rename columns of each dataframe
 newnames <- c("Time", "PercInf")
 output.list <- lapply(output.list, setNames, newnames)
-head(output.list)
+head(output.list[[1]])
 
+#Add column to denote replicates
+replicate <- logical()
+for(i in 1:50){
+  new <- rep(i, 1000)
+  replicate <- append(replicate, new)
 
-
-
-
-    
+output.list <- lapply(output.list, cbind, replicate)    
