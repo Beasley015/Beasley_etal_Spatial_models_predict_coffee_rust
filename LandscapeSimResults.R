18b8ccdaba72e49cf908e7e4723bb23a2ed80940
eribue <42782508+eribue@users.noreply.github.com>
2019-04-29 15:07:58 -0400

    ok

--- LandscapeSimResults.R
+++ LandscapeSimResults.R
@@ -4,25 +4,44 @@
 # Spring 2019                                       #
 #####################################################
 
-#Libraries
-library(tidyverse)
-library(ggplot2)
-
 # Read in model outputs --------------------------------------
 # Read all csv's into a list
 filenames <- list.files("Outputs", pattern = "*.csv", full.names = T)
-shortnames <- list.files("Outputs", pattern = "*.csv")
 output.list <- lapply(filenames, read.csv, header = F)
 
 # Rename columns of each dataframe
 newnames <- c("Time", "PercInf")
 output.list <- lapply(output.list, setNames, newnames)
-head(output.list[[1]])
 
-#Add column to denote replicates
-replicate <- logical()
-for(i in 1:50){
-  new <- rep(i, 1000)
-  replicate <- append(replicate, new)
+#Libraries
+library(tidyverse)
+library(ggplot2)
+#adding columns defors and disp
+defors <- c("10")
+disp<- c("2")
+for( i in seq_along(output.list)){
+  output.list[[i]]$defors <- rep(defors[i],nrow(output.list[[i]]))
+  output.list[[i]]$disp<-rep(disp[i], nrow(output.list[[i]]))
+  }
+print(output.list[[1]])
 
-output.list <- lapply(output.list, cbind, replicate)    
+library(stringr)
+is.atomic(as.matrix(as.data.frame(output.list)))
+str_which(output.list,"def")
+def<- c("10")
+disp<- c("2", "3", "4", "5")
+str_match_all(output.list, c(def, disp))
+
+str_extract_all(is.atomic(as.matrix(as.data.frame(output.list))), "disp")
+
+?str_extract_all
+
+
+for( i in seq_along(output.list)){
+  output.list[[i]]$def <- str_extract_all(output.list[i],c("def10", "disp"))
+  
+}
+head()
+
+
+    
