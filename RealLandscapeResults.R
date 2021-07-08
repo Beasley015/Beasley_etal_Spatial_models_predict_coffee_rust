c73d4ca3fe8ee556c74f000f8a7465ad31ed0014
Emily Beasley <41756876+Beasley015@users.noreply.github.com>
2021-07-08 16:03:03 -0400

    R script for real landscapes

---
+++ RealLandscapeResults.R
@@ -0,0 +1,44 @@
+##########################################################
+# Modeling coffee rust movement through a real landscape #
+# N. Aristizabal, E. Beasley, E. Bueno, & E. White       #
+# Summer 2021- ????                                      #
+##########################################################
+
+# Load packages and data ------------------------------
+# Packages
+library(landscapemetrics)
+library(rgdal)
+library(raster)
+library(tidyverse)
+
+# Raw raster data
+x <- new("GDALReadOnlyDataset", "./coffeeRust_landuses")
+getDriver(x)
+getDriverLongName(getDriver(x))
+xx<-asSGDF_GROD(x)
+bothlands <- raster(xx)
+
+# Format rasters ------------------------------
+# Convert to matrix
+rawmat <- is.na(as.matrix(bothlands))
+
+# Get cols that are all NA
+colNA <- which(colSums(rawmat) == nrow(bothlands))
+
+# Trim both rasters
+land1extent <- extent(bothlands, 1, nrow(rawmat), 1, colNA[1]-1)
+land1raw <- crop(bothlands, land1extent)
+
+land2extent <- extent(bothlands, 1, nrow(rawmat), colNA[3]+1,ncol(rawmat))
+land2raw <- crop(bothlands, land2extent)
+
+# Extract coffee values
+land1 <- land1raw == 3
+land1[land1 == 0] <- NA
+
+land2 <- land2raw == 3
+land2[land2 == 0] <- NA
+
+# Calculate aggregation index -------------------
+lsm_l_ai(land1)
+lsm_l_ai(land2)
